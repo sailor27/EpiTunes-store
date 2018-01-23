20336bf9cddd2e0143daa113c320d3f40e1d99bd
Holly Wilkalis and Sailor Winkelman <sailor@>
2018-01-23 14:05:57 -0800

    Set up albumservice to connect to Firebase. Add admin page with submission form. Call getAlbums to pull sata to marketplace from database. Call addAlbum on form submit to add album to database.

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
-    albums: Album[];
-  constructor( ) { }
+    albums: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
+  }
 
   getAlbums() {
-    return ALBUMS;
+    return this.albums;
+  }
+
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
   }
 
   getAlbumById(albumId: number){
-    for (var i = 0; i<= ALBUMS.length - 1; i++) {
-      if (ALBUMS[i].id === albumId) {
-        return ALBUMS[i];
-      }
-    }
+    // for (var i = 0; i<= ALBUMS.length - 1; i++) {
+    //   if (ALBUMS[i].id === albumId) {
+    //     return ALBUMS[i];
+    //   }
+    // }
   }
 
 
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
@@ -11,27 +12,17 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
   constructor(private router: Router, private albumService: AlbumService) {}
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
   }
 
-
-
   goToDetailPage(clickedAlbum: Album) {
-     this.router.navigate(['albums', clickedAlbum.id]);
+    //  this.router.navigate(['albums', clickedAlbum.id]);
   };
 
-  @Output() newAlbumSender = new EventEmitter();
 
-// dont do it this way
-  // submitForm(title, artist, description, id) {
-  //   var newAlbumToAdd: Album = new Album(title, artist, description, id);
-  //   this.newAlbumSender.emit(newAlbumToAdd);
-  //   console.log(newAlbumToAdd);
-  //   albums.addAlbum()
+
   }
-
-}
